d1b533f6a7929d2235648a6d4261c4a6b60e30a3
Elvis Omoro <iamomoroit@gmail.com>
2022-08-26 23:18:39 +0300

    styling-set

--- src/star-rating/StarRating.jsx
+++ src/star-rating/StarRating.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  stars: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  textarea: {
+    border: "1px solid #a9a9a9",
+    borderRadius: 5,
+    padding: 10,
+    margin: "20px 0",
+    minHeight: 100,
+    width: 300
+  },
+  button: {
+    border: "1px solid #a9a9a9",
+    borderRadius: 5,
+    width: 300,
+    padding: 10,
+  }
+
+};
+
+
 const StarRating = () => {
   return (
     <div>StarRating</div>
